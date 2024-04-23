8535141cdc9f89d47807ebe18269ca6e098c95f0
MarwaAbuEssa <meromeroo@gmail.com>
2024-04-24 01:10:30 +0200

    basic , async , mesure

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -6,9 +6,9 @@ import random
 from typing import Generator
 
 
-async def async_generator() -> Generator[float, None, None]: # type: ignore
-    """ Generates a sequence of 10 numbers.
-    """
+async def async_generator() -> Generator[float, None, None]:
+    '''Generates a sequence of 10 numbers.
+    '''
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.random() * 10
